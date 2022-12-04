0de2035a4496d0b70b3816a784edec204a33d2e2
Dan Hamilton <hamiltondan93@gmail.com>
2022-12-04 18:24:20 +0000

    Add: tasks added to app

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,9 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
 
-  document.addEventListener('DOMContentLoaded', function() {
+    // delete cats initialization
     let delModal = document.querySelectorAll('.modal');
     M.Modal.init(delModal);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker,{
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
   });
